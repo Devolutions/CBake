0918a647b5f89d0c59eb882cb69edb43fd283081
Marc-André Moreau <marcandre.moreau@gmail.com>
2019-04-25 16:25:30 -0400

    cbake: add Linux cross-compilation toolchains

---
+++ linux-amd64.toolchain.cmake
@@ -0,0 +1,4 @@
+set(CMAKE_SYSTEM_PROCESSOR x86_64)
+set(CMAKE_SIZEOF_VOID_P 8)
+
+include("${CMAKE_CURRENT_LIST_DIR}/linux.toolchain.cmake")
---
+++ linux-i386.toolchain.cmake
@@ -0,0 +1,5 @@
+set(CMAKE_SYSTEM_PROCESSOR i686)
+set(CMAKE_SIZEOF_VOID_P 4)
+
+include("${CMAKE_CURRENT_LIST_DIR}/linux.toolchain.cmake")
+
