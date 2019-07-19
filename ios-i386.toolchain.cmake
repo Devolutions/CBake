0b9ff64c154f02c2e1966c126d13e6c3681f571c
Richard Markiewicz <rmarkiewicz@devolutions.net>
2019-07-19 12:20:48 -0700

    cbake: remove ios-i386 toolchain

--- ios-i386.toolchain.cmake
+++
@@ -1,7 +0,0 @@
-set(CMAKE_SYSTEM_NAME "iOS")
-set(CMAKE_SYSTEM_VERSION 8.2)
-set(IOS_DEPLOYMENT_TARGET "${CMAKE_SYSTEM_VERSION}")
-set(CMAKE_OSX_ARCHITECTURES "i386")
-
-include("${CMAKE_CURRENT_LIST_DIR}/ios-common.cmake")
-include("${CMAKE_CURRENT_LIST_DIR}/ios.toolchain.cmake")
