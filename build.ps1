5e0c8b0f39565477f4ef0a035d734ef409941259
Marc-André Moreau <mamoreau@devolutions.net>
2021-08-25 15:50:11 -0400

    update build script

--- build.ps1
+++ build.ps1
@@ -2,4 +2,19 @@
 
 Import-Module "$Env:CBAKE_HOME/cbake.psm1" -Force
 
-New-CBakeSysroot @args
+$CmdVerbs = @('sysroot')
+
+if ($args.Count -lt 1) {
+    throw "not enough arguments!"
+}
+
+$CmdVerb = $args[0]
+$CmdArgs = $args[1..$args.Count]
+
+if ($CmdVerbs -NotContains $CmdVerb) {
+    throw "invalid verb $CmdVerb, use one of: [$($CmdVerbs -Join ',')]"
+}
+
+switch ($CmdVerb) {
+    "sysroot" { New-CBakeSysroot @CmdArgs }
+}
