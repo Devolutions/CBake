f36bc0918c4739e38f4e09431f4a76ccb7ae8e39
Marc-André Moreau <marcandre.moreau@gmail.com>
2021-09-28 14:02:16 -0400

    Update Windows toolchain files (#16)
    * update windows MSVC toolchain
    
    * refactor and improve Windows cross-compilation toolchain
    
    * update windows toolchain file
    
    * update windows toolchain files
    
    Co-authored-by: Marc-André Moreau <mamoreau@devolutions.net>

--- cmake/windows-arm64.toolchain.cmake
+++ cmake/windows-arm64.toolchain.cmake
@@ -1,7 +1,5 @@
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR aarch64)
-
-set(HOST_ARCH "x64")
+set(CMAKE_SYSTEM_PROCESSOR arm64)
 set(TARGET_ARCH "arm64")
 
-include("${CMAKE_CURRENT_LIST_DIR}/windows-msvc.toolchain.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/windows.toolchain.cmake")
--- cmake/windows-x86.toolchain.cmake
+++ cmake/windows-x86.toolchain.cmake
@@ -1,7 +1,5 @@
 set(CMAKE_SYSTEM_NAME Windows)
-set(CMAKE_SYSTEM_PROCESSOR i686)
-
-set(HOST_ARCH "x64")
+set(CMAKE_SYSTEM_PROCESSOR x86)
 set(TARGET_ARCH "x86")
 
-include("${CMAKE_CURRENT_LIST_DIR}/windows-msvc.toolchain.cmake")
+include("${CMAKE_CURRENT_LIST_DIR}/windows.toolchain.cmake")
